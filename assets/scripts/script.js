9f3caf3b0583a601c2af00cbb4bc49acbdd93ba2
Sid <siddhant.khare.sk@gmail.com>
2022-09-14 23:31:54 +0530

    Second last Commit before validation, gallery and about us

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -35,3 +35,63 @@ const signbutton = document.querySelector(".signbutt");
     login.addEventListener("click", ( )=>{
         container.classList.remove("active");
     });
+
+    function validate_form() {
+        var name = document.forms["RegForm"]["Fullname"];
+        var lname = document.forms["RegForm"]["LName"];
+        var mname = document.forms["RegForm"]["MName"];
+        var email = document.forms["RegForm"]["Email"];
+        var phone = document.forms["RegForm"]["Phone"];
+        var sid = document.forms["RegForm"]["Sid"];
+        var dropdown = document.forms["RegForms"]["Dropdown"]
+
+        if (name.value == "") {
+            window.alert("Please enter your First name: ");
+            fname.focus();
+            return false;
+        }
+
+        if (lname.value == "") {
+            window.alert("Please enter your Last name: ");
+            lname.focus();
+            return false;
+        }
+        if (mname.value == "") {
+            window.alert("Please enter your Middle name: ");
+            mname.focus();
+            return false;
+        }
+
+        if (email.value == "") {
+            window.alert("Please enter a valid Email Address: ");
+            email.focus();
+            return false;
+        }
+        if (email.value.indexOf("@", 0) < 0) {
+            window.alert("Please enter a valid Email Address: ");
+            email.focus();
+            return false;
+        }
+        if (email.value.indexOf(".", 0) < 0) {
+            window.alert("Please enter a valid Email Address: ");
+            email.focus();
+            return false;
+        }
+        if (phone.value == "") {
+            window.alert("Please enter a valid Phone: ");
+            phone.focus();
+            return false;
+        }
+        if (sid.value == "") {
+            window.alert("Please enter your SID No. : ");
+            sid.focus();
+            return false;
+        }
+        if (dropdown.selectedIndex < 1) {
+            window.alert("Please select a stream: ");
+            dropdown.focus();
+            return false;
+        }
+        return true;
+
+    }
